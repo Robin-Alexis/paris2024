a16ddd4a7334327b64a049838d1c51efa534123c
alexis <alexisrob@netc.fr>
2024-09-09 13:26:40 +0200

    Création de la class Epreuve avec la relation  Many to one

---
+++ src/main/java/sio/paris2024/model/Epreuve.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sio.paris2024.model;
+
+/**
+ *
+ * @author SIO2
+ */
+public class Epreuve {
+    
+    private int id;
+    private String nom ;
+    private Sport sport;
+
+    public Epreuve() {
+    }
+
+    public Epreuve(int id, String nom) {
+        this.id = id;
+        this.nom = nom;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public Sport getSport() {
+        return sport;
+    }
+
+    public void setSport(Sport sport) {
+        this.sport = sport;
+    }      
+}
